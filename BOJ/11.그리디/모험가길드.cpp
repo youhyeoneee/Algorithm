777157366b4f11b0c7d91006efa913e6f15d33ae
youhyeoneee <khukyh2018@khu.ac.kr>
2022-06-12 14:24:12 +0900

    220612

--- BOJ/11.그리디/모험가길드.cpp
+++ BOJ/11.그리디/모험가길드.cpp
@@ -6,23 +6,21 @@ using namespace std;
 
 int main(){
     
-    int N, result = 0, cnt = 0; 
-    // 모험가의 수, 그룹의 수, 현재 그룹안에 있는 모험가의 수
-
+    int N, result = 0, cnt = 0, x;
     cin >> N;
-
     vector<int> v;
 
     for(int i=0; i<N; i++){
-        cin >> v[i];
+        cin >> x;
+        v.push_back(x);
     }
 
     sort(v.begin(), v.end());
         
      for(int i=0; i<N; i++){
-         cnt ++;
+         cnt++;
          if (cnt >= i){
-            result ++;
+            result++;
             cnt = 0;            
          }
      }
