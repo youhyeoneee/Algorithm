0cd8612e2c63dc6908dfe632fd1de58655a8f6e8
Kim Yuhyeon <khukyh2018@khu.ac.kr>
2023-05-06 00:15:19 +0900

    230506

---
+++ BOJ/단계별로 풀어보기/06.문자열/27866.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+    string S;
+    int i;
+
+    cin >> S;
+    cin >> i;
+
+    cout << S[i-1] << '\n';
+
+
+
+
+    return 0;
+}
