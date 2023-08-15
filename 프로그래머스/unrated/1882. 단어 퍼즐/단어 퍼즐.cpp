82df7f848a2684bdfaa9ffec02c879ee4a4ae0e8
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2023-08-15 20:57:00 +0900

    [unrated] Title: 단어 퍼즐, Time: 20.58 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/unrated/1882. 단어 퍼즐/단어 퍼즐.cpp
+++ 프로그래머스/unrated/1882. 단어 퍼즐/단어 퍼즐.cpp
@@ -1,91 +1,72 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+#define INF 987654321
+#define MAX 20004
+
 using namespace std;
 
-vector<pair<string, int>> dp[20004]; 
+int dp[MAX]; 
 
+void Init()
+{
+    for(int i=1; i<MAX; i++)
+    {
+        dp[i] = INF;
+    }
+}
 
 int solution(vector<string> strs, string t)
 {
-    int answer = 0;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
     
-    // 문자열 길이대로 넣기 
-    for(string str : strs)
+    // 배열 초기화 
+    Init();
+    
+    int answer = -1;
+    
+    // 1부터 t의 길이까지 부분 문자열을 뽑는다.
+    for(int len=1; len<=t.length(); len++)
     {
-        dp[str.length()].push_back({str, 1});
-    }
-
-    // dp 실행
-    // 0부터 문자열 길이까지
-    for(int i=1; i<t.length(); i++)
-    {
-        // i = 2면
         
-            
-        for(int j=0; j<i; j++)
+        string subStr = t.substr(0, len);
+        
+        // 단어 조각
+        for(string word : strs)
         {
-                    for(auto : dp[n-i])
-            // 2 = {2} + 0
-            // 1 = {1} + {1}
-            dp[i] = dp[i-j] + dp[j];
+            // 시작 인덱스 (ex. bana(4) - na(2) - 2 > 0)
+            int startIdx = len - word.length(); 
             
+            // 길이가 크면 패스
+            if (startIdx < 0)
+                continue;
             
+            // 길이가 작거나 같으면 완전히 같은 지 검사
+            // ba"na" == "na" ? 
+            bool isSame = true;
+            
+            for(int i=0; i<word.length(); i++)
+            {
+                if (subStr[startIdx + i] != word[i])
+                {
+                    isSame = false;
+                    break;
+                }
+            }
+            
+            // 같지 않으면 패스
+            if (!isSame)
+                continue;
+                        
+            dp[len] = min(dp[len], dp[startIdx] + 1);
         }
-        
-        
     }
  
+	
     
-
+    // 갱신이 되었다면 리턴 
+    if (dp[t.length()] != INF)
+        answer = dp[t.length()];
+    
     return answer;
 }
-
-// strs 1~100개 
-// banana 
-// ba + na + na = 3개
-// ba + na + n + a = 4개
-// ba + n + a + n + a = 5개
-
-// app, ple, ap, pp, p, l, e
-// apple
-// app + l + e 
-// ple + ap 
-// ap + p + l + e
-// p + l + e + ap
-// l + app + e
-// l + ap + p + e
-//
-
-
-
-// dp 인 것 같다 ..
-
-
-// 5 = 4+1 = 3+2 = 2+3 = 1+4
-
-// 4 = 3+1 = 2+2 = 1+3
-// 4+0 : X
-// 3+1 : ba + n + a (3), na + n + a (3), n + a + n + a (4), a + n + a + n (4), a + na + n (3)
-// 2+2 : ba + na (2), ba + n + a (3), na + n + a (3), n + a + na (3), ...
-// 1+3 : n + a + n + a (4) 
-
-// 3 = 2+1 = 1+2 (string 내에 없거나, 이미 있으면 없애기)
-// 3+0 : X
-// 2+1 : ba + n (2) , na + n (2) , n + a + n (3), a + n + a (3)
-// 1+2 : a + na (2)
-
-// 2 = 2+0 = 1+1
-// 2+0 : ba (1), na (1)
-// 1+1 = n + a (2), a + n (2)
-
-// 1 = 1+0
-// 1개짜리 : n (1) , a (1)
-
-// a + p + p + l + e
-// ap + p + l + e
-// ap + pl + e
-// ap + p + le
