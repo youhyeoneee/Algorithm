c647bd0a62bf041e94b005c31767cf90e5f09036
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2023-08-15 21:55:31 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/lv4/12929. 올바른 괄호의 갯수/README.md
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/README.md
@@ -1,10 +1,10 @@
 # [level 4] 올바른 괄호의 갯수 - 12929 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12929) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12929#) 
 
 ### 성능 요약
 
-메모리: 4.02 MB, 시간: 7118.86 ms
+메모리: 4.18 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
@@ -1,56 +1,26 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
-#include <stack>
 
 using namespace std;
 
-vector<char> charVec;
+int dp[15];
 
-bool IsRightGwalho()
-{
-    stack<char> ss;
+
+int solution(int n) {
     
-    for(char ch : charVec)
-    {
-        if (ss.empty()) 
-            ss.push(ch);
-        else
+    int answer = 0;
+   
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(int i=2; i<=n; i++)
+    {        
+        int mmax = i-1;
+        
+        for(int j=0; j<=mmax; j++)
         {
-            if (ss.top() == '(' && ch == ')')   
-                ss.pop();
-            else
-                ss.push(ch);
+             dp[i] += dp[mmax-j] * dp[j];
         }
     }
-    
-    return ss.empty();
-}
-int solution(int n) {
-    int answer = 0;
-    
-    // 0. base가 되는 문자열 생성
-    
-    for(int i=0; i<n; i++)
-    {
-        charVec.push_back('(');
-        charVec.push_back(')');
-    }
-    
-    
-    // 정렬
-    sort(charVec.begin(), charVec.end());
-    
-    // 1. 순열 구하기 
-    do
-    {
-        // 2. 올바른 괄호인지 check 
-        if (IsRightGwalho())
-            answer++;
-            
-    }while(next_permutation(charVec.begin(), charVec.end()));
-    
-    
-    return answer;
+    return dp[n];
 }
