dfbed8cb0458670b8e8b4eafcdc39af84b27d9e9
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2023-08-15 20:41:08 +0900

    [unrated] Title: 단어 퍼즐, Time: 18.62 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/unrated/1882. 단어 퍼즐/단어 퍼즐.cpp
+++ 프로그래머스/unrated/1882. 단어 퍼즐/단어 퍼즐.cpp
@@ -1,75 +1,91 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-#define INF 987654321
-#define MAX 20004
-
 using namespace std;
 
-int dp[MAX]; 
+vector<pair<string, int>> dp[20004]; 
 
-void Init()
-{
-    for(int i=1; i<MAX; i++)
-    {
-        dp[i] = INF;
-    }
-}
 
 int solution(vector<string> strs, string t)
 {
-    
-    // 배열 초기화 
-    Init();
-    
-    int answer = -1;
-    
-    // 1. 점화식을 cache[t의 인덱스] = min(cache[t의 인덱스], cache[t의 인덱스 - 원소 길이] + 1)로 설정해줍니다.
-    
-    // 1.1 여기서 cache[t의 인덱스]란 t의 인덱스까지의 부분 문자열을 뽑았을 때 단어 조각들의 최소 숫자입니다.
+    int answer = 0;
 
-    // 2. 이중 반복문을 통해 위 점화식을 구해줍니다.
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
     
-    for(int len=1; len<=t.length(); len++)
+    // 문자열 길이대로 넣기 
+    for(string str : strs)
     {
+        dp[str.length()].push_back({str, 1});
+    }
+
+    // dp 실행
+    // 0부터 문자열 길이까지
+    for(int i=1; i<t.length(); i++)
+    {
+        // i = 2면
         
-        string subStr = t.substr(0, len);
-        
-        // 단어 조각
-        for(string word : strs)
+            
+        for(int j=0; j<i; j++)
         {
-            // 시작 인덱스 (ex. bana(4) - na(2) - 2 > 0)
-            int startIdx = len - word.length(); 
-            // 길이가 크면 패스
-            if (startIdx < 0)
-                continue;
+                    for(auto : dp[n-i])
+            // 2 = {2} + 0
+            // 1 = {1} + {1}
+            dp[i] = dp[i-j] + dp[j];
             
-            // 길이가 작거나 같으면 완전히 같은 지 검사
-            // ba"na" == "na" ? 
-            bool isSame = true;
-            for(int i=0; i<word.length(); i++)
-            {
-                if (subStr[startIdx + i] != word[i])
-                {
-                    isSame = false;
-                    break;
-                }
-            }
             
-            // 같지 않으면 패스
-            if (!isSame)
-                continue;
-                        
-            dp[len] = min(dp[len], dp[startIdx] + 1);
         }
+        
+        
     }
  
-    // 3. cache[t의 길이]가 INF라면 문자열을 단어 조각들로 형성 못하므로 -1을 반환해주고, 그 외 케이스는 cache[t의 길이]를 반환해줍니다.
-    // 3.1 cache[t의 길이] = t의 길이까지의 부분 문자열을 이루는 단어 조각들의 최소 숫자 = t를 이루는 단어 조각들의 최소 숫자
     
-    if (dp[t.length()] != INF)
-        answer = dp[t.length()];
-    
+
     return answer;
 }
+
+// strs 1~100개 
+// banana 
+// ba + na + na = 3개
+// ba + na + n + a = 4개
+// ba + n + a + n + a = 5개
+
+// app, ple, ap, pp, p, l, e
+// apple
+// app + l + e 
+// ple + ap 
+// ap + p + l + e
+// p + l + e + ap
+// l + app + e
+// l + ap + p + e
+//
+
+
+
+// dp 인 것 같다 ..
+
+
+// 5 = 4+1 = 3+2 = 2+3 = 1+4
+
+// 4 = 3+1 = 2+2 = 1+3
+// 4+0 : X
+// 3+1 : ba + n + a (3), na + n + a (3), n + a + n + a (4), a + n + a + n (4), a + na + n (3)
+// 2+2 : ba + na (2), ba + n + a (3), na + n + a (3), n + a + na (3), ...
+// 1+3 : n + a + n + a (4) 
+
+// 3 = 2+1 = 1+2 (string 내에 없거나, 이미 있으면 없애기)
+// 3+0 : X
+// 2+1 : ba + n (2) , na + n (2) , n + a + n (3), a + n + a (3)
+// 1+2 : a + na (2)
+
+// 2 = 2+0 = 1+1
+// 2+0 : ba (1), na (1)
+// 1+1 = n + a (2), a + n (2)
+
+// 1 = 1+0
+// 1개짜리 : n (1) , a (1)
+
+// a + p + p + l + e
+// ap + p + l + e
+// ap + pl + e
+// ap + p + le
