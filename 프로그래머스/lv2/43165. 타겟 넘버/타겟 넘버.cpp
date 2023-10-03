4edc70003a531f082bd859bb54c61b7b61c0afb0
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2023-10-03 18:54:39 +0900

    [level 2] Title: 타겟 넘버, Time: 0.04 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -4,39 +4,26 @@
 
 using namespace std;
 
-int n;
 int answer = 0;
 
-void Dfs(int idx, int num, bool sign, vector<int> &numbers, int &target)
-{
-
-    // 부호에 따라 숫자 더하기, 빼기
-    if (idx >= 0 && idx < n) 
-    {
-        if (sign) num += numbers[idx];
-        else num -= numbers[idx];
-    }
-    
+void Dfs(int idx, int sum, vector<int> &numbers, int &target)
+{    
     // 기저 사례 : 마지막 숫자까지 처리
-    if (idx == n-1)
+    if (idx == numbers.size())
     {
-        if (num == target) answer++;        
+        if (sum == target) answer++;        
         return;
     }
            
-    // 더하기
-    Dfs(idx+1, num, true, numbers, target);
-    // 빼기 
-    Dfs(idx+1, num, false, numbers, target);
+    Dfs(idx+1, sum + numbers[idx], numbers, target);  // 더하기
+    Dfs(idx+1, sum - numbers[idx], numbers, target);  // 빼기 
 
 }
 
 int solution(vector<int> numbers, int target) 
 {
-    
-    n = numbers.size();
-    
-    Dfs(-1, 0, true, numbers, target);
+        
+    Dfs(0, 0, numbers, target);
     
     return answer;
 }
