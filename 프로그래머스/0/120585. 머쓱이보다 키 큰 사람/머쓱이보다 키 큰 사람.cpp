97e55f36bbc194b05e08022ae3d80b393d5a8a0b
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-04-11 00:16:51 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    
+    for(int i : array) {
+        if (i > height) answer++;
+    }
+    
+    return answer;
+}
