1a9f2e7eb40c06e82910e364e88bf0fdf1e71117
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-04-15 00:10:16 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.44 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -4,6 +4,6 @@ class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
         
-        return array[(array.length-1)/2];
+        return array[array.length/2];
     }
 }
