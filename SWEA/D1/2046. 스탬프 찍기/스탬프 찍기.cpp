587063609517b527a90d808dc781d00e097e2b1b
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-05-18 23:50:26 +0900

    [D1] Title: 스탬프 찍기, Time: 6 ms, Memory: 13,336 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n;
+	cin >> n;
+
+	for(int i=0; i<n; i++)
+	{
+        cout << "#";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
