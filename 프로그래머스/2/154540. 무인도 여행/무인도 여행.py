8e6bec5cdd05269640fb9441938da70ed69c7538
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-10-16 11:16:14 +0900

    [level 2] Title: 무인도 여행, Time: 10.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,42 +1,42 @@
 from collections import deque
 
-def is_valid(y: int, x: int, n: int, m: int):
-    return 0 <= y < n and 0 <= x < m
-
 def solution(maps): 
     answer = []
+    
     # n 세로 * m 가로
-    m = len(maps[0])
     n = len(maps)
-
+    m = len(maps[0])
+    
+    # 방문 체크 
     visited = [[False] * m for _ in range(n)]
-    direct = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
     
     def bfs(y, x):
         q = deque()
         q.append((y, x))
         visited[y][x] = True 
-        cnt = 0
+        # 머물 수 있는 일수
+        days = 0
         
         while q:
             cy, cx = q.popleft()
-            cnt += int(maps[cy][cx])
+            days += int(maps[cy][cx])
             
-            for dy, dx in direct:
-                ny = cy + dy
-                nx = cx + dx
-                    
-                if is_valid(ny, nx, n, m) and maps[ny][nx] != 'X' and not visited[ny][nx]:          
+            for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                ny, nx = cy + dy, cx + dx
+                if 0 <= ny < n and 0 <= nx < m:
+                    if maps[ny][nx] == 'X' or visited[ny][nx]:  
+                        continue 
                     q.append((ny, nx))
                     visited[ny][nx] = True 
         
-        return cnt 
+        return days 
     
     
     for y in range(n):
         for x in range(m):
-            if maps[y][x] != 'X' and not visited[y][x]:
-                answer.append(bfs(y, x))
+            if maps[y][x] == 'X' or visited[y][x]:
+                continue
+            answer.append(bfs(y, x))
     
     if answer:
         answer.sort()
