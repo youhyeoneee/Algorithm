3c620facb9e40481ccb2c9c58e409baf77e82a1d
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-10-17 15:20:03 +0900

    [level 3] Title: 양과 늑대, Time: 6.50 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,25 +1,35 @@
+def solution(info, edges):
+    from collections import defaultdict
+    graph = defaultdict(list)
     
-def solution(info, edges):    
-    visited = [False] * len(info)
-    answer = []
-
-    def dfs(sheep, wolf):
-        if sheep > wolf:
-            answer.append(sheep)
+    # 그래프 구성
+    for parent, child in edges:
+        graph[parent].append(child)
+    
+    # DFS 정의
+    def dfs(node, sheep, wolf, possible_nodes):
+        nonlocal answer
+        if info[node] == 0:
+            sheep += 1
         else:
-            return 
+            wolf += 1
         
-        for p, c in edges:
-            if visited[p] and not visited[c]:
-                visited[c] = True
-                if info[c]:
-                    dfs(sheep, wolf+1)
-                else:
-                    dfs(sheep+1, wolf)
-                visited[c] = False
+        # 늑대가 양을 넘으면 더 이상 탐색하지 않음
+        if wolf >= sheep:
+            return
+        
+        # 최대 양의 수 갱신
+        answer = max(answer, sheep)
+        
+        # 이동 가능한 노드 복제 (다른 경로에 영향 안 미치도록)
+        possible_nodes = possible_nodes[:]
+        possible_nodes.remove(node)
+        possible_nodes.extend(graph[node])
+        
+        # 다음 가능한 정점들로 이동
+        for next_node in possible_nodes:
+            dfs(next_node, sheep, wolf, possible_nodes)
     
-    # 루트 노드부터 시작
-    visited[0] = True
-    dfs(1, 0)
-    
-    return max(answer)
+    answer = 0
+    dfs(0, 0, 0, [0])  # 시작은 0번 정점
+    return answer
