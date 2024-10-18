ce87091d859c300197ff36e2874ab3df103b2e5a
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-10-18 13:31:58 +0900

    [level 2] Title: 숫자 변환하기, Time: 470.73 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,38 +1,44 @@
-from collections import deque, defaultdict
 
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
 def solution(x, y, n):
-    
-    if y == x:
-        return 0
-    
-    answer = int(1e9)
-    visited = defaultdict(bool)
-    
-    q = deque()
-    q.append((y, 0))
-    
-    while q:
-        num, cnt = q.popleft()
-        
-        if num == x:
-            answer = min(answer, cnt)
-        
-        if num < x or cnt > answer:
-            continue
-        
-        if num - n >= x and not visited[num - n]:
-            q.append((num - n, cnt + 1))
-            visited[num - n] = True 
-        
-        if num % 3 == 0 and not visited[num // 3]:
-            q.append((num // 3, cnt + 1))
-            visited[num // 3] = True 
+    answer = 0
+    s = set()
+    s.add(x)
 
-        if num % 2 == 0 and not visited[num // 2]:
-            q.append((num // 2, cnt + 1))
-            visited[num // 2] = True 
-            
-    if answer == int(1e9):
-        return -1 
-    
-    return answer
+    while s:
+        if y in s:
+            return answer
+
+        nxt = set()
+        for i in s:
+            if i+n <= y:
+                nxt.add(i+n)
+            if i*2 <= y:
+                nxt.add(i*2)
+            if i*3 <= y:
+                nxt.add(i*3)
+        s = nxt
+        answer+=1
+
+    return -1
