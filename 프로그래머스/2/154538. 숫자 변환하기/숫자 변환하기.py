89cbdc94bf952f20e4a944f048d8e7d814dde366
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-10-18 13:29:06 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 
 def solution(x, y, n):
     
@@ -6,27 +6,31 @@ def solution(x, y, n):
         return 0
     
     answer = int(1e9)
+    visited = defaultdict(bool)
     
     q = deque()
     q.append((y, 0))
     
     while q:
         num, cnt = q.popleft()
-
+        
         if num == x:
             answer = min(answer, cnt)
         
         if num < x or cnt > answer:
             continue
         
-        if num - n >= x:
+        if num - n >= x and not visited[num - n]:
             q.append((num - n, cnt + 1))
+            visited[num - n] = True 
         
-        if num % 3 == 0:
+        if num % 3 == 0 and not visited[num // 3]:
             q.append((num // 3, cnt + 1))
+            visited[num // 3] = True 
 
-        if num % 2 == 0:
+        if num % 2 == 0 and not visited[num // 2]:
             q.append((num // 2, cnt + 1))
+            visited[num // 2] = True 
             
     if answer == int(1e9):
         return -1 
