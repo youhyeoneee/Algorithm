aa0588226fa63713dbcad5b925c9210e40b82cda
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-10-18 14:48:41 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 180.93 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,4 +1,5 @@
 def solution(sequence):
     v = [0]
-    for i,s in enumerate(sequence): v.append(v[-1] + s * [1,-1][i%2])
-    return max(v) - min(v)
+    for i,s in enumerate(sequence): 
+        v.append(v[-1] + s * [1,-1][i%2])
+    return abs(max(v) - min(v))
