e55dddf736a971c4b64ad38ecd17e2c9a6b38c81
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-11-16 23:35:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,24 +1,11 @@
 class Solution {
     public int solution(int num) {
-        int answer = 0;
-        long num2 = num;
-        
-        do {               
-            if (num2 == 1)
-                break;
-            
-            answer++;    
-            if (num2 % 2 == 0) 
-                num2 /= 2;
-            else
-                num2 = num2 * 3 + 1;
-        } while(answer < 500);
-        
-        if (answer == 500) 
-            return -1;
-        
-        return answer;
-        
-    
+        long n = (long)num;
+        for(int i=0; i<500; i++){
+            if(n == 1) return i; 
+            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
+        }
+
+        return -1;
     }
 }
